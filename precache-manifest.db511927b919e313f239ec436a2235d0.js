f4880490944049394102e997e9487365594e7368
ejazsayyad <ejazsayyad4@gmail.com>
2020-03-30 21:01:49 +0530

    Updates

---
+++ precache-manifest.db511927b919e313f239ec436a2235d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7f2502457414944e5bb667a1e1e193",
+    "url": "/ImagesearchApp/index.html"
+  },
+  {
+    "revision": "98a8f7840d75e3c08bc9",
+    "url": "/ImagesearchApp/static/css/main.9b96c28d.chunk.css"
+  },
+  {
+    "revision": "8881ebeb4ade05f14099",
+    "url": "/ImagesearchApp/static/js/2.6c774b5d.chunk.js"
+  },
+  {
+    "revision": "157f3c50781e053edfcdbff9e61cbc24",
+    "url": "/ImagesearchApp/static/js/2.6c774b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a8f7840d75e3c08bc9",
+    "url": "/ImagesearchApp/static/js/main.1ee6fe5a.chunk.js"
+  },
+  {
+    "revision": "060c7b8ba0212648930b",
+    "url": "/ImagesearchApp/static/js/runtime-main.c5cbc121.js"
+  }
+]);
